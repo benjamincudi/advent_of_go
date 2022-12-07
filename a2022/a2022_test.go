f60e334c75bb55f43d176129878dba705503db9e
Ben Garfield <bengarfield@gmail.com>
2022-12-06 23:58:36 -0500

    alternate, faster solution for day 6
    - spend more memory to do even more of a sliding window
    and have an even faster check for if we're done searching
    - benchmarks to prove the speed is real, this is 25% faster

--- a2022/a2022_test.go
+++ a2022/a2022_test.go
@@ -166,4 +166,49 @@ func Test_day6(t *testing.T) {
 			}
 		})
 	}
+	testCases = []struct {
+		name                          string
+		in                            io.Reader
+		startOfPacket, startOfMessage int
+	}{
+		{"control case 1", strings.NewReader(control[0]), 7, 19},
+		{"control case 2", strings.NewReader(control[1]), 5, 23},
+		{"control case 3", strings.NewReader(control[2]), 6, 23},
+		{"control case 4", strings.NewReader(control[3]), 10, 29},
+		{"control case 5", strings.NewReader(control[4]), 11, 26},
+		{"personal input", mustOpen(t, "day6.txt"), 1598, 2414},
+	}
+	for _, tc := range testCases {
+		t.Run(fmt.Sprintf("fast - %s", tc.name), func(t *testing.T) {
+			if r := day6fast(tc.in); r.startOfPacket != tc.startOfPacket {
+				t.Errorf("expected start-of-packet %d, got %d", tc.startOfPacket, r.startOfPacket)
+			} else if r.startOfMessage != tc.startOfMessage {
+				t.Errorf("expected start-of-message %d, got %d", tc.startOfMessage, r.startOfMessage)
+			}
+		})
+	}
+}
+
+func Benchmark_day6(b *testing.B) {
+	reader := bufio.NewScanner(mustOpen(b, "control6.txt"))
+	var testCases []string
+	for reader.Scan() {
+		testCases = append(testCases, reader.Text())
+	}
+	reader = bufio.NewScanner(mustOpen(b, "day6.txt"))
+	for reader.Scan() {
+		testCases = append(testCases, reader.Text())
+	}
+	modulo := len(testCases)
+	b.Run("day6 normal", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			day6(strings.NewReader(testCases[i%modulo]))
+		}
+	})
+	b.Run("day6 fast", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			day6fast(strings.NewReader(testCases[i%modulo]))
+		}
+	})
+}
 }
--- a2022/day6.go
+++ a2022/day6.go
@@ -2,6 +2,7 @@ package a2022
 
 import (
 	"bufio"
+	"errors"
 	"io"
 	"strings"
 )
@@ -21,6 +22,51 @@ type res struct {
 	startOfPacket, startOfMessage int
 }
 
+type overlapChecker struct {
+	duplicates, length int
+	state              map[string]int
+	history            []string
+}
+
+func newOverlapChecker(l int) overlapChecker {
+	return overlapChecker{length: l, state: map[string]int{}}
+}
+
+func (o *overlapChecker) initFrom(src []string) error {
+	if len(src) != o.length {
+		return errors.New("initFrom src must match overlapChecker length")
+	}
+	o.history = src
+	for _, s := range src {
+		if o.state[s] > 0 {
+			o.duplicates++
+		}
+		o.state[s]++
+	}
+	return nil
+}
+
+func (o *overlapChecker) next(s string) {
+	if o.state[s] > 0 {
+		o.duplicates++
+	}
+	o.state[s]++
+	if len(o.history) == o.length {
+		popped, rest := o.history[0], o.history[1:]
+		o.history = append(rest, s)
+		o.state[popped]--
+		if o.state[popped] > 0 {
+			o.duplicates--
+		}
+	} else if len(o.history) < o.length {
+		o.history = append(o.history, s)
+	}
+}
+
+func (o *overlapChecker) done() bool {
+	return len(o.history) == o.length && o.duplicates == 0
+}
+
 func day6(in io.Reader) res {
 	r := bufio.NewScanner(in)
 	r.Scan()
@@ -32,6 +78,10 @@ func day6(in io.Reader) res {
 			break
 		}
 	}
+	// if we didn't find an answer, a longer answer can't exist
+	if sop == -1 {
+		return res{-1, -1}
+	}
 	for i := maxInt(sop+10, 14); i <= len(d); i++ {
 		if !anyOverlap(d[i-14 : i]) {
 			som = i
@@ -41,5 +91,43 @@ func day6(in io.Reader) res {
 	return res{
 		startOfPacket:  sop,
 		startOfMessage: som,
+	}
+}
+
+func day6fast(in io.Reader) res {
+	r := bufio.NewScanner(in)
+	r.Scan()
+	d := strings.Split(r.Text(), "")
+	sop, som := -1, -1
+	sopChecker, somChecker := newOverlapChecker(4), newOverlapChecker(14)
+	if err := sopChecker.initFrom(d[0:4]); err != nil {
+		panic(err)
+	}
+	for i := 4; i < len(d); i++ {
+		sopChecker.next(d[i])
+		if sopChecker.done() {
+			sop = i
+			break
+		}
+	}
+	// if we didn't find an answer, a longer answer can't exist
+	if sop == -1 {
+		return res{-1, -1}
+	}
+	somStartingI := maxInt(sop+10, 14)
+	if err := somChecker.initFrom(d[somStartingI-14 : somStartingI]); err != nil {
+		panic(err)
+	}
+	for i := somStartingI; i < len(d); i++ {
+		somChecker.next(d[i])
+		if somChecker.done() {
+			som = i
+			break
+		}
+	}
+	return res{
+		// answers are 1-indexed
+		startOfPacket:  sop + 1,
+		startOfMessage: som + 1,
 	}
 }
