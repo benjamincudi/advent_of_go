5c870991a1044008ccd59695505b69d98c3c4aa4
Ben Garfield <bengarfield@gmail.com>
2024-12-07 20:47:40 -0500

    day 4 part 2
    - instructions unclear, also found plus-sign MAS initially which gave more hits than intended

--- a2024/day04.go
+++ a2024/day04.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"image"
 	"io"
+	"slices"
 	"strings"
 )
 
@@ -14,67 +15,60 @@ func day4(r io.Reader) (int, int) {
 		lines = append(lines, strings.Split(scanner.Text(), ""))
 	}
 
-	finder := makeFinder(lines)
+	finder := func(p image.Point) int {
+		target := "XMAS"
+		found := 0
 
-	var xmasCount int
+		for dx := -1; dx < 2; dx++ {
+			for dy := -1; dy < 2; dy++ {
+				found += aElseB(sliceFromPoint(lines, len(target), p, dx, dy) == target, 1, 0)
+			}
+		}
+
+		return found
+	}
+
+	crossFinder := func(p image.Point) int {
+		targets := []string{"MAS", "SAM"}
+		found := 0
+
+		d1 := sliceFromPoint(lines, 3, image.Point{X: p.X - 1, Y: p.Y - 1}, 1, 1)
+		d2 := sliceFromPoint(lines, 3, image.Point{X: p.X - 1, Y: p.Y + 1}, 1, -1)
+
+		if slices.Contains(targets, d1) && slices.Contains(targets, d2) {
+			found++
+		}
+		return found
+	}
+
+	xmasCount := 0
+	crossCount := 0
 	for y, row := range lines {
 		for x, letter := range row {
 			if letter == "X" {
 				xmasCount += finder(image.Point{X: x, Y: y})
 			}
+			if letter == "A" {
+				crossCount += crossFinder(image.Point{X: x, Y: y})
+			}
 		}
 	}
 
-	return xmasCount, 0
+	return xmasCount, crossCount
 }
 
-func makeFinder(lines [][]string) func(p image.Point) int {
+func sliceFromPoint(lines [][]string, length int, p image.Point, dX, dY int) string {
 	mX, mY := len(lines[0])-1, len(lines)-1
-	sj := func(s []string) string {
-		if len(s) != 4 {
-			panic("isn't right length")
-		}
-		return strings.Join(s, "")
-	}
-	xmas := "XMAS"
 
-	sliceFromPoint := func(p image.Point, dX, dY int) string {
-		var ret []string
-		for i := 0; i < 4; i++ {
-			ret = append(ret, lines[p.Y][p.X])
-			p.X += dX
-			p.Y += dY
+	var ret []string
+	for i := 0; i < length; i++ {
+		if p.X < 0 || p.Y < 0 || p.X > mX || p.Y > mY {
+			return strings.Join(ret, "")
 		}
-		return sj(ret)
+		ret = append(ret, lines[p.Y][p.X])
+		p.X += dX
+		p.Y += dY
 	}
 
-	return func(p image.Point) int {
-		found := 0
-		if p.X+3 <= mX {
-			found += aElseB(sliceFromPoint(p, 1, 0) == xmas, 1, 0)
-			if p.Y >= 3 {
-				found += aElseB(sliceFromPoint(p, 1, -1) == xmas, 1, 0)
-			}
-			if p.Y+3 <= mY {
-				found += aElseB(sliceFromPoint(p, 1, 1) == xmas, 1, 0)
-			}
-		}
-		if p.X >= 3 {
-			found += aElseB(sliceFromPoint(p, -1, 0) == xmas, 1, 0)
-			if p.Y >= 3 {
-				found += aElseB(sliceFromPoint(p, -1, -1) == xmas, 1, 0)
-			}
-			if p.Y+3 <= mY {
-				found += aElseB(sliceFromPoint(p, -1, 1) == xmas, 1, 0)
-			}
-		}
-		if p.Y >= 3 {
-			found += aElseB(sliceFromPoint(p, 0, -1) == xmas, 1, 0)
-		}
-		if p.Y+3 <= mY {
-			found += aElseB(sliceFromPoint(p, 0, 1) == xmas, 1, 0)
-		}
-
-		return found
-	}
+	return strings.Join(ret, "")
 }
