d212136b14b9fae4cafd2c0eb03bf1fb36fc652d
Ben Garfield <bengarfield@gmail.com>
2024-12-08 12:55:29 -0500

    day 6 part 2
    - its so slow, 6s can't be good

--- a2024/day06.go
+++ a2024/day06.go
@@ -10,7 +10,7 @@ import (
 func day6(r io.Reader) (int, int) {
 	scanner := bufio.NewScanner(r)
 	obstacles := map[image.Point]struct{}{}
-	var currentLoc image.Point
+	var startingLoc image.Point
 	maxY := -1
 	maxX := 0
 	for scanner.Scan() {
@@ -22,28 +22,70 @@ func day6(r io.Reader) (int, int) {
 				obstacles[image.Point{X: currentX, Y: maxY}] = struct{}{}
 			}
 			if char == "^" {
-				currentLoc = image.Point{X: currentX, Y: maxY}
+				startingLoc = image.Point{X: currentX, Y: maxY}
 			}
 		}
 
 	}
 
-	visitedPoints := map[image.Point]struct{}{currentLoc: {}}
-	walkingDirections := [][]int{{0, -1}, {1, 0}, {0, 1}, {-1, 0}}
+	type direction image.Point
+	walkingDirections := []direction{{0, -1}, {1, 0}, {0, 1}, {-1, 0}}
+	getDir := func(index int) direction { return walkingDirections[index%4] }
+
 	walkingIndex := 0
+	currentLoc := startingLoc
+	visitedPoints := map[image.Point]struct{}{startingLoc: {}}
 	for {
-		d := walkingDirections[walkingIndex]
-		nextStep := image.Point{X: currentLoc.X + d[0], Y: currentLoc.Y + d[1]}
+		d := getDir(walkingIndex)
+		nextStep := image.Point{X: currentLoc.X + d.X, Y: currentLoc.Y + d.Y}
 		if _, blocked := obstacles[nextStep]; blocked {
-			walkingIndex = (walkingIndex + 1) % len(walkingDirections)
+			walkingIndex++
 			continue
 		}
 		if nextStep.X < 0 || nextStep.X > maxX || nextStep.Y < 0 || nextStep.Y > maxY {
 			break
 		}
-		visitedPoints[nextStep] = struct{}{}
 		currentLoc = nextStep
+		visitedPoints[currentLoc] = struct{}{}
 	}
 
-	return len(visitedPoints), 0
+	hasCycle := func() bool {
+		currentLoc = startingLoc
+		cycleWalkingIndex := 0
+		cycleVisits := map[image.Point]map[direction]struct{}{startingLoc: {getDir(cycleWalkingIndex): {}}}
+		for {
+			d := getDir(cycleWalkingIndex)
+			nextStep := image.Point{X: currentLoc.X + d.X, Y: currentLoc.Y + d.Y}
+			if _, blocked := obstacles[nextStep]; blocked {
+				cycleWalkingIndex++
+				continue
+			}
+			if nextStep.X < 0 || nextStep.X > maxX || nextStep.Y < 0 || nextStep.Y > maxY {
+				return false
+			}
+			if dirs, visited := cycleVisits[nextStep]; visited {
+				if _, sameDir := dirs[d]; sameDir {
+					return true
+				}
+			} else {
+				cycleVisits[nextStep] = map[direction]struct{}{}
+			}
+			cycleVisits[nextStep][d] = struct{}{}
+			currentLoc = nextStep
+		}
+	}
+
+	cycleCount := 0
+	for theoreticalObstruction := range visitedPoints {
+		if theoreticalObstruction == startingLoc {
+			continue
+		}
+		obstacles[theoreticalObstruction] = struct{}{}
+		if hasCycle() {
+			cycleCount++
+		}
+		delete(obstacles, theoreticalObstruction)
+	}
+
+	return len(visitedPoints), cycleCount
 }
