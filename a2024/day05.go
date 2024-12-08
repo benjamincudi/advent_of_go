35edb44e8782f671041b29f7a2fe2e425ac2921b
Ben Garfield <bengarfield@gmail.com>
2024-12-07 21:39:15 -0500

    day 5 part 2
    - silly easy, thanks sorting

--- a2024/day05.go
+++ a2024/day05.go
@@ -3,6 +3,7 @@ package a2024
 import (
 	"bufio"
 	"io"
+	"slices"
 	"strings"
 )
 
@@ -26,6 +27,8 @@ func day5(r io.Reader) (int, int) {
 	validUpdates := 0
 	middleSums := 0
 
+	var invalidUpdates [][]int
+
 updateLoop:
 	for _, update := range updates {
 		seen := map[int]struct{}{}
@@ -33,6 +36,7 @@ updateLoop:
 			if pageRules, mustBeBefore := rules[page]; mustBeBefore {
 				for _, mustBeAfter := range pageRules {
 					if _, invalidUpdate := seen[mustBeAfter]; invalidUpdate {
+						invalidUpdates = append(invalidUpdates, update)
 						continue updateLoop
 					}
 				}
@@ -43,5 +47,23 @@ updateLoop:
 		middleSums += update[(len(update)-1)/2]
 	}
 
-	return middleSums, 0
+	fixedMiddleSums := 0
+	for _, update := range invalidUpdates {
+		slices.SortFunc(update, func(a, b int) int {
+			if pageRules, ok := rules[a]; ok {
+				if slices.Contains(pageRules, b) {
+					return -1
+				}
+			}
+			if pageRules, ok := rules[b]; ok {
+				if slices.Contains(pageRules, a) {
+					return 1
+				}
+			}
+			return 0
+		})
+		fixedMiddleSums += update[(len(update)-1)/2]
+	}
+
+	return middleSums, fixedMiddleSums
 }
