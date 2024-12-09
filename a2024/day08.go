2683f1a391f2cc5391b38ef4b12031252e2f354e
Ben Garfield <bengarfield@gmail.com>
2024-12-08 23:19:32 -0500

    day 8 part 2
    - could definitely do it in one pass but why bother

--- a2024/day08.go
+++ a2024/day08.go
@@ -79,5 +79,42 @@ func day8(r io.Reader) (int, int) {
 		}
 	}
 
-	return hasAntinode, 0
+	for label, locs := range labelLocations {
+		for i, left := range locs {
+			for j := i + 1; j < len(locs); j++ {
+				right := locs[j]
+				gridInfo[left].antinodeFor(label)
+				gridInfo[right].antinodeFor(label)
+				distanceToJ := right.Sub(left)
+				for pastRight := right.Add(distanceToJ); pastRight.In(gridBox); pastRight = pastRight.Add(distanceToJ) {
+					if _, seen := gridInfo[pastRight]; !seen {
+						n := &antennaPoint{}
+						n.antinodeFor(label)
+						gridInfo[pastRight] = n
+					} else {
+						gridInfo[pastRight].antinodeFor(label)
+					}
+				}
+				distanceFromJ := left.Sub(right)
+				for beforeLeft := left.Add(distanceFromJ); beforeLeft.In(gridBox); beforeLeft = beforeLeft.Add(distanceFromJ) {
+					if _, seen := gridInfo[beforeLeft]; !seen {
+						n := &antennaPoint{}
+						n.antinodeFor(label)
+						gridInfo[beforeLeft] = n
+					} else {
+						gridInfo[beforeLeft].antinodeFor(label)
+					}
+				}
+			}
+		}
+	}
+
+	resonanceAntinode := 0
+	for _, p := range gridInfo {
+		if len(p.antinodes) > 0 {
+			resonanceAntinode++
+		}
+	}
+
+	return hasAntinode, resonanceAntinode
 }
